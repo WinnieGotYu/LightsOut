147abca31ffc46bee9c506d4ea035dc7921a394d
WinnieGotYu <WinnieGotYu@gmail.com>
2019-11-06 19:11:36 -0800

    Changed word out to off

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           Lights Out <i className="far fa-lightbulb"></i>
         </h1>
         <h4>
-          Instructions: The goal is to turn out all the lights, ideally with the
+          Instructions: The goal is to turn off all the lights, ideally with the
           minimum number of clicks. Clicking on a cell toggles that cell and
           each of its immediate neighbors.
         </h4>
